3e964a06ab2edcc407a46de1f1feef5e36134f7b
sbsejal <100947132+sbsejal@users.noreply.github.com>
2022-03-04 19:28:25 +0530

    Add files via upload

---
+++ calculator1/Form1.vb
@@ -0,0 +1,152 @@
+﻿Public Class Form1
+    Dim Firstnum As Decimal
+    Dim Secondnum As Decimal
+    Dim Operations As Decimal
+    Dim Operator_Selector As Boolean = False
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "1"
+        Else
+            TextBox1.Text = "1"
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "2"
+        Else
+            TextBox1.Text = "2"
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "3"
+        Else
+            TextBox1.Text = "3"
+        End If
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "4"
+        Else
+            TextBox1.Text = "4"
+        End If
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "5"
+        Else
+            TextBox1.Text = "5"
+        End If
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "6"
+        Else
+            TextBox1.Text = "6"
+        End If
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "7"
+        Else
+            TextBox1.Text = "7"
+        End If
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "8"
+        Else
+            TextBox1.Text = "8"
+        End If
+    End Sub
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "9"
+        Else
+            TextBox1.Text = "9"
+        End If
+    End Sub
+
+    Private Sub Button13_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button13.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "0"
+       
+        End If
+    End Sub
+
+    Private Sub Button15_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button15.Click
+        TextBox1.Text = "0"
+    End Sub
+
+    Private Sub Button14_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button14.Click
+        If Not (TextBox1.Text.Contains(".")) Then
+            TextBox1.Text += "."
+        End If
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Firstnum = TextBox1.Text
+        TextBox1.Text = "0"
+        Operator_Selector = True
+        Operations = 1 ' = +
+
+
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        Firstnum = TextBox1.Text
+        TextBox1.Text = "0"
+        Operator_Selector = True
+        Operations = 2 ' = -
+
+    End Sub
+
+    Private Sub Button12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button12.Click
+        Firstnum = TextBox1.Text
+        TextBox1.Text = "0"
+        Operator_Selector = True
+        Operations = 3 ' = *
+    End Sub
+
+    Private Sub Button16_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button16.Click
+        Firstnum = TextBox1.Text
+        TextBox1.Text = "0"
+        Operator_Selector = True
+        Operations = 4 ' = /
+    End Sub
+
+    Private Sub Button17_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button17.Click
+        If Operator_Selector = True Then
+            Secondnum = TextBox1.Text
+            If Operations = 1 Then
+                TextBox1.Text = Firstnum + Secondnum
+            ElseIf Operations = 2 Then
+                TextBox1.Text = Firstnum - Secondnum
+            ElseIf Operations = 3 Then
+                TextBox1.Text = Firstnum * Secondnum
+            Else
+                If Secondnum = 0 Then
+                    TextBox1.Text = "ERROR !"
+                Else
+                    TextBox1.Text = Firstnum / Secondnum
+                End If
+
+            End If
+            Operator_Selector = False
+
+        End If
+    End Sub
+End Class
